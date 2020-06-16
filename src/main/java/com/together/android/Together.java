80b697384b3b6cfc59d2a030e4f5f536e4c38a04
Douglas Lara <douglasdelaras@gmail.com>
2020-06-15 22:37:27 -0300

    first commit

---
+++ src/main/java/com/together/android/Together.java
@@ -0,0 +1,13 @@
+package com.together.android;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Together {
+
+    public static void modal(Context c){
+
+        Toast.makeText(c,"Exibe modal",Toast.LENGTH_SHORT).show();
+
+    }
+}
